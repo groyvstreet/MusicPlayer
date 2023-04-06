90144e8977448310ec8c995150619b7ed7e416b5
groyvstreet <kirya.polyakov.2003@mail.ru>
2023-04-06 15:06:15 +0300

    Info about a current track, back, forward, repeat buttons are working now in the player

--- Lab3/js/player.js
+++ Lab3/js/player.js
@@ -1,5 +1,75 @@
+let tracks = [
+    {
+        id: "1",
+        title: "Gimme the loot",
+        image: "https://www.teachhub.com/wp-content/uploads/2019/10/Our-Top-10-Songs-About-School-768x569.png",
+        src: "js/track.mp3"
+    },
+    {
+        id: "2",
+        title: "Song",
+        image: "https://play-lh.googleusercontent.com/QovZ-E3Uxm4EvjacN-Cv1LnjEv-x5SqFFB5BbhGIwXI_KorjFhEHahRZcXFC6P40Xg",
+        src: "https://cdn8.sefon.pro/prev/TivTlpcRzwyZ7uBiqN-FYA/1680822107/393/Александр%20Кендысь%20feat.%20W.J.Rec%20-%20Кинолента%20%28192kbps%29.mp3"
+    },
+    {
+        id: "3",
+        title: "Песня",
+        image: "https://online.berklee.edu/takenote/wp-content/uploads/2022/01/how_to_write_a_love_song_article_image_2022-1.png",
+        src: "https://cdn3.sefon.pro/prev/mfW6CHdvatwtULfZO2bHsw/1680822107/394/Artik%20%26%20Asti%20-%20Кукла%20%28192kbps%29.mp3"
+    },
+];
+
+let trackIndex = 0;
 let isTrackPlaying = false;
-let audio = new Audio("js/track.mp3");
+let toRepeat = false;
+let audio = new Audio(tracks[trackIndex].src);
+
+function updatePlayerTrack(track) {
+    var image = document.getElementById("player-cover-image");
+    image.src = track.image;
+
+    var title = document.getElementById("player-title");
+    title.innerHTML = track.title;
+}
+
+function selectPreviousTrack() {
+    trackIndex -= 1;
+
+    if (trackIndex < 0) {
+        trackIndex = tracks.length - 1;
+    }
+
+    audio.src = tracks[trackIndex].src;
+    isTrackPlaying = false;
+    updatePlayerImagePlay();
+    isTrackPlaying = true;
+    audio.play();
+}
+
+function selectNextTrack() {
+    trackIndex += 1;
+
+    if (trackIndex > tracks.length - 1) {
+        trackIndex = 0;
+    }
+
+    audio.src = tracks[trackIndex].src;
+    isTrackPlaying = false;
+    updatePlayerImagePlay();
+    isTrackPlaying = true;
+    audio.play();
+}
+
+function updatePlayerImagePlay() {
+    var image = document.getElementById("player-img-play");
+
+    if (isTrackPlaying) {
+        image.src = "img/play.svg";
+    }
+    else {
+        image.src = "img/pause.svg";
+    }
+}
 
 audio.addEventListener("timeupdate", function() {
     let currentTime = Math.trunc(audio.currentTime);
@@ -12,9 +82,22 @@ audio.addEventListener("timeupdate", function() {
     let currentSeconds = currentTime % 60;
     document.getElementById("player-time-current").innerHTML = currentSeconds < 10 ? `${currentMinutes}:0${currentSeconds}` : `${currentMinutes}:${currentSeconds}`;
 
-    let durationMinutes = Math.trunc(duration / 60);
-    let durationSeconds = duration % 60;
-    document.getElementById("player-time").innerHTML = durationSeconds < 10 ? `${durationMinutes}:0${durationSeconds}` : `${durationMinutes}:${durationSeconds}`;
+    if (duration.toString() != "NaN") {
+        let durationMinutes = Math.trunc(duration / 60);
+        let durationSeconds = duration % 60;
+        document.getElementById("player-time").innerHTML = durationSeconds < 10 ? `${durationMinutes}:0${durationSeconds}` : `${durationMinutes}:${durationSeconds}`;
+    }
+
+    updatePlayerTrack(tracks[trackIndex]);
+
+    if (currentTime == duration) {
+        if (toRepeat) {
+            audio.currentTime = 0;
+        }
+        else {
+            selectNextTrack();
+        }
+    }
 });
 
 document.getElementById("player-button-play").addEventListener("click", function() {
@@ -28,18 +111,25 @@ document.getElementById("player-button-play").addEventListener("click", function
     }
 });
 
-document.getElementById("player-img-play").addEventListener("click", function() {
-    var image = document.getElementById("player-img-play");
-
-    if (isTrackPlaying) {
-        image.src = "img/play.svg";
-    }
-    else {
-        image.src = "img/pause.svg";
-    }
-});
+document.getElementById("player-img-play").addEventListener("click", updatePlayerImagePlay);
 
 document.getElementById("player-bar").addEventListener("click", function (event) {
     playerBar = document.getElementById("player-bar");
     audio.currentTime = audio.duration * event.offsetX / playerBar.offsetWidth;
+});
+
+document.getElementById("player-back-button").addEventListener("click", selectPreviousTrack);
+document.getElementById("player-forward-button").addEventListener("click", selectNextTrack);
+document.getElementById("player-button-repeat").addEventListener("click", function() {
+    toRepeat = !toRepeat;
+});
+document.getElementById("player-img-repeat").addEventListener("click", function() {
+    var image = document.getElementById("player-img-repeat");
+
+    if (toRepeat) {
+        image.src = "img/repeat.svg";
+    }
+    else {
+        image.src = "img/repeat-active.svg";
+    }
 });
