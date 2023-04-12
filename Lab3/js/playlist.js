edb2aa1561f74bb39e722c05a205aa8a7d1fefab
groyvstreet <kirya.polyakov.2003@mail.ru>
2023-04-12 17:09:27 +0300

    Lab4 updated

--- Lab3/js/playlist.js
+++ Lab3/js/playlist.js
@@ -15,4 +15,12 @@ user.then((u) => {
         tracks = playlist.tracks.filter(track => track.title.toLowerCase().includes(event.target.value.toLowerCase()));
         getTracks(tracks);
     });
+
+    document.getElementById("preview-button-delete").addEventListener("click", async () => {
+        let response = await fetch(`https://krakensound-ee3a2-default-rtdb.firebaseio.com/users/${localStorage.getItem("user_id")}/playlists/${id}.json`, {method: "delete"});
+        
+        if (response.ok) {
+            window.location.href = 'playlists.html';
+        }
+    });
 });
